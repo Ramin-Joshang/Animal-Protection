5aeca3dfffcac7e117b2c80dd5c2e35a9f54d822
Ramin Joshang <rjwshng@gmail.com>
2021-12-13 21:04:01 +0330

    Site header section completed

--- js/script.js
+++ js/script.js
@@ -1,33 +1,13 @@
-const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-btn");
-const hamburgerBtn = document.querySelector(".hamburger-btn");
-const menu = document.querySelector(".right-list");
-const banner = document.querySelector(".banner");
+var rellax = new Rellax('.rellax');
 
-// navbar searchBox
+const navbar = document.querySelector(".navbar");
+const a = navbar.querySelectorAll("a");
 
-searchBtn.addEventListener("click", () => {
-    searchInput.value = "";
-    searchInput.focus();
-});
-
-
-// window.onload = () => {
-//     if (window.innerWidth <= 992) {
-//         banner.style.background = "url(/images/Capture2.PNG)"
-//     }
-//     if (window.innerWidth <= 700) {
-//         banner.style.background = "url(/images/slide.jpg)"
-//     } else {
-
-//     }
-// }
-
-
-// responsive menu
-hamburgerBtn.onclick = () => {
-    menu.classList.add("active");
-}
-banner.onclick = () => {
-    menu.classList.remove("active");
-}
+a.forEach(item => {
+    item.addEventListener("click", () => {
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove("active");
+        }
+        item.classList.add("active");
+    })
+})
